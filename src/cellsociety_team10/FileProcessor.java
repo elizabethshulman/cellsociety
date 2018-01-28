42d62b81713394e37c7d3cd16d2008bc0fe43ae5
Andrew Yeung <ahy7@duke.edu>
2018-01-28 17:06:30 -0500

    Added instance variables and methods to complete FileProcessor abstract class

--- src/cellsociety_team10/FileProcessor.java
+++ src/cellsociety_team10/FileProcessor.java
@@ -1,5 +1,26 @@
 package cellsociety_team10;
 
-public class FileProcessor {
+public abstract class FileProcessor {
+	private String filepath;
+	private String author;
+	private String title;
+	private Cell[][] grid;
+	public FileProcessor(){
+		
+	}
+	public String getFilepath() {
+		return filepath;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public Cell[][] getGrid()
+	{
+		return grid;
+	}
+	public abstract void readFile(String file);
 
 }

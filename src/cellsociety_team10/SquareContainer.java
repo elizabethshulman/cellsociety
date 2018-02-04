ac74ab34dd25035ec737feb2d56cd681d3054241
Ben Hubsch <bah37@duke.edu>
2018-02-03 19:30:53 -0500

    Fixing bugs in grid animation.

--- src/cellsociety_team10/SquareContainer.java
+++ src/cellsociety_team10/SquareContainer.java
@@ -1,17 +1,15 @@
 package cellsociety_team10;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import cellVariants.Cell;
 import graphVariants.Graph;
+import javafx.geometry.Pos;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class SquareContainer extends Container {
 	private static final double GRID_SIZE = 400;
-	private static final double n = 9;
+	private static final int n = 8;
 	
 	private VBox myVBox;
 	private VBox myDisplay;
@@ -42,35 +40,37 @@ public class SquareContainer extends Container {
 //		return hbox;
 //	}
 //	
-//	
-//	
-//	public void setGraphDisplay(HashMap<Cell, ArrayList<Cell>> cell_map) {
+//	public void setGraphDisplay(Graph g) {
 //		myDisplay.getChildren().clear();
 //		for (int r=0; r < n; r++) {
 //			myDisplay.getChildren().add(buildGraphRow(r));
 //		}
 //	}
 	
+	private HBox buildGraphRow(ImageView[][] graph_grid, int r, int num_cols) {
+		HBox row = new HBox();
+		for (int c=0; c < num_cols; c++) {
+			row.getChildren().add(graph_grid[r][c]);
+		}
+		row.setAlignment(Pos.CENTER);
+		row.setMaxWidth(0);
+		return row;
+	}
+	
 	public void setGraphDisplay(Graph g) {
 		myDisplay.getChildren().clear();
 		ImageView[][] graph_grid = mapToGrid(g);
-		for (int c=0; c < g.getCols(); c++) {
-			HBox row = new HBox();
-			row.setId("container-row");
-			for (int r=0; r < g.getRows() / 4; r++) {
-				row.getChildren().add(graph_grid[r][c]);
-			}
-			myDisplay.getChildren().add(row);
+		for (int r=0; r < g.getRows(); r++) {
+			myDisplay.getChildren().add(buildGraphRow(graph_grid, r, g.getCols()));
 		}
 	}
 	
 	private ImageView[][] mapToGrid(Graph g) {
-//		need to think about this line...what if it's not a perfect square?
 		ImageView[][] curr_grid = new ImageView[g.getRows()][g.getCols()];
 		for (Cell curr : g.getCells()) {
 			ImageView curr_image_view = curr.getImageView();
 			curr_image_view.setPreserveRatio(true);
-			curr_image_view.setFitHeight((GRID_SIZE - 2*2) / n);
+			curr_image_view.setFitHeight((GRID_SIZE - 2 * 2) / g.getCols());
 			curr_grid[curr.getRow()][curr.getCol()] = curr_image_view;
 		}
 		return curr_grid;

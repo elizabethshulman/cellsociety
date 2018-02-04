334c748ee655f5f50ac4c6cdf0cf2b1289c55bde
Elizabeth Shulman <ers58@duke.edu>
2018-02-03 19:31:11 -0500

    Run simulation called

--- src/rulesVariants/FireRules.java
+++ src/rulesVariants/FireRules.java
@@ -17,18 +17,18 @@ import cellVariants.Cell;
  */
 public class FireRules extends Rules {
 	
-	private final double probCatch;
+	private final double probCatchFire;
 	private Random randomGenerator = new Random();
 	
 	public FireRules(HashMap<String,Double> globalVars) {
-		probCatch = globalVars.get("probCatch"); //confirm with Andrew	
+		probCatchFire = globalVars.get("probCatchFire"); //confirm with Andrew	
 	}
 	
 	@Override
 	protected Boolean dissatisfied(int state, ArrayList<Cell> neighbors) {
 		return(state==2 || 
 				(state==1 && neighborIsBurning(neighbors) 
-				&& (randomGenerator.nextInt(100)+1)>probCatch));
+				&& (randomGenerator.nextInt(100)+1)>probCatchFire));
 	}
 
 	/**

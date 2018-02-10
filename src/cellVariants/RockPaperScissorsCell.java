a33080947815755fe9f1504fa09bd205388a19ad
Elizabeth Shulman <ers58@duke.edu>
2018-02-10 16:32:31 -0500

    Revised post merge errors

--- src/cellVariants/RockPaperScissorsCell.java
+++ src/cellVariants/RockPaperScissorsCell.java
@@ -6,28 +6,25 @@ public class RockPaperScissorsCell extends Cell{
 
 	private int health;
 	
-	public RockPaperScissorsCell(int st) {
-		super(st);
-		if(st==0) {
-			health=0;
-		} else {
-			health = new Random().nextInt(10);
-		}
+	public RockPaperScissorsCell(int st, String shape) {
+		super(st, shape);
+		health = new Random().nextInt(10);
 	}
 
 	@Override
 	protected void buildHashMap() {
-		statesAndColors.put(0, buildCellImage("black.png"));
-		statesAndColors.put(1, buildCellImage("darkblue.png"));
-		statesAndColors.put(2, buildCellImage("forestgreen.png"));
-		statesAndColors.put(3, buildCellImage("lightteal.png"));
+		statesAndImages.put(0, buildCellImage(getShapeType() + "/black.png"));
+		statesAndImages.put(1, buildCellImage(getShapeType() + "/sharkred.png"));
+		statesAndImages.put(2, buildCellImage(getShapeType() + "/forestgreen.png"));
+		statesAndImages.put(3, buildCellImage(getShapeType() + "/lightteal.png"));
+		
+		///ADD STATESANDCOLORS FOR GRAPH
 	}
 
 	public void increaseHealth() {
 		if (health<=10) {
 			health+=1;
 		}
-		
 	}
 
 	public void decreaseHealth() {

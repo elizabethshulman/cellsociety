821d631696d2a09fc37588b2c653344345e9d942
Andrew Yeung <ahy7@duke.edu>
2018-02-10 16:38:49 -0500

    Added ability to save simulations where cells have multiple attributes

--- src/fileInfoExtractorVariants/FileInfoExtractor.java
+++ src/fileInfoExtractorVariants/FileInfoExtractor.java
@@ -2,11 +2,12 @@ package fileInfoExtractorVariants;
 
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 
 import cellVariants.Cell;
 
 public interface FileInfoExtractor {
 	public Double getGlobalVar(XMLStreamReader xmlRead) throws XMLStreamException;
 	public Cell getCell(XMLStreamReader xmlRead, String shape) throws XMLStreamException;
-	public String getEncoding(int state);
+	public void writeCell(XMLStreamWriter myWriter, Cell cell) throws XMLStreamException;
 }

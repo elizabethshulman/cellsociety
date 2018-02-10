ec99cb4eddb864660be037542ad85d6359a2d011
Ben Hubsch <bah37@duke.edu>
2018-02-09 23:02:42 -0500

    Finished styling Sidebar.

--- src/visualComponents/Sidebar.java
+++ src/visualComponents/Sidebar.java
@@ -11,12 +11,14 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.util.StringConverter;
 
 public class Sidebar {
@@ -34,7 +36,12 @@ public class Sidebar {
 		myVBox.setId("sidebar");
 		Slider rows = createSlider();
 		Slider cols = createSlider();
-		myVBox.getChildren().addAll(buildSimBox(resources), rows, cols, buildShapeOptions(resources));
+
+		textAndComponent(resources.getString("PickSim"), buildSimBox(resources));
+		textAndComponent(resources.getString("Width"), rows);
+		textAndComponent(resources.getString("Height"), cols);
+		textAndComponent(resources.getString("Shape"), buildShapeOptions(resources));
+		textAndComponent(resources.getString("Shape"), buildEdgeBox(resources));
 		
 		rows.setOnMouseReleased(e -> {
 			graph.adjustRows((int) rows.getValue());
@@ -43,6 +50,20 @@ public class Sidebar {
 		cols.setOnMouseReleased(e -> {
 			graph.adjustCols((int) cols.getValue());
 		});
+	}
+
+	private void textAndComponent(String text, Node node) {
+		VBox pair = new VBox();
+		pair.setId("pairing");
+		Text temp = new Text(text);
+		temp.setId("text-sidebar");
+		
+		HBox left_align = new HBox();
+		left_align.setId("left-align");
+		left_align.getChildren().add(temp);
+		
+		pair.getChildren().addAll(left_align, node);
+		myVBox.getChildren().add(pair);
 	}
 
 	public VBox getVBox() {
@@ -71,6 +92,26 @@ public class Sidebar {
 		return combo;
 	}
 	
+	private ComboBox<String> buildEdgeBox(ResourceBundle resources) {
+		ObservableList<String> edge_options = FXCollections.observableArrayList(
+				resources.getString("FiniteEdge"),
+				resources.getString("ToroidalEdge")
+				);
+		
+		ComboBox<String> combo = new ComboBox<String>(edge_options);
+		combo.setId("combo");
+		combo.getSelectionModel().selectFirst();
+		combo.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+	        @Override
+	        public void changed(ObservableValue<? extends String> ov,
+	        		String old_val, String new_val) {
+	        		System.out.println(new_val);;
+			}
+	    });
+
+		return combo;
+	}
+	
 	private Map<String, String> initTranslations(ResourceBundle resources) {
 		HashMap<String, String> translations = new HashMap<>();
 		translations.put(resources.getString("LifeButton"), "data/simulations/life/gameoflife1.xml");
@@ -83,10 +124,14 @@ public class Sidebar {
 	private HBox buildShapeOptions(ResourceBundle resources) {
 		HBox hbox = new HBox();
 		hbox.setId("shape-hbox");
-		RadioButton rb1 = createRadioButton("burgundy.png");
-		RadioButton rb2 = createRadioButton("triangle1.png");
-		RadioButton rb3 = createRadioButton("hex1.png");
+		RadioButton rb1 = createRadioButton("sidebar_square.png");
+		rb1.setId("radio");
 		rb1.setSelected(true);
+		RadioButton rb2 = createRadioButton("sidebar_triangle.png");
+		rb2.setId("radio");
+		RadioButton rb3 = createRadioButton("sidebar_hex.png");
+		rb3.setId("radio");
+		
 		
 		ToggleGroup toggleGroup = new ToggleGroup();
 		rb1.setToggleGroup(toggleGroup);
@@ -106,14 +151,15 @@ public class Sidebar {
 	private Slider createSlider() {
 		Slider temp = new Slider(MIN_SLIDER, MAX_SLIDER, START_SLIDER);
 		temp.getStyleClass().add("axis");
-		temp.setId("#slider-sidebar");
+		temp.setId("slider-sidebar");
+
 		temp.setLabelFormatter(new StringConverter<Double>() {
 			@Override
 			public String toString(Double n) {
 				if (n == MIN_SLIDER) {
-					return "4";
+					return "  4";
 				} else if (n == MAX_SLIDER) {
-					return "40";
+					return "40   ";
 				}
 				return "";
 			}
